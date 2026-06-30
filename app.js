a79594d6c107e3b5c908b628225e7629e39bf026
Gamtwe <ellulchri@gmail.com>
2026-06-30 11:38:10 +0200

    Update app.js - min max temp

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ async function fetchWeather(location = "Luqa,Malta") {
         updateText('wind-dir', getWindDirection(current.winddir));
         updateText('wind-bft', `${getBeaufort(current.windspeed)} BFT`);
         updateText('moon-phase', getMoonPhaseName(todayData.moonphase));
-                // Add these lines inside your fetchWeather function under Basic Info:
         updateText('today-max', `${Math.round(todayData.tempmax)}°C`);
         updateText('today-min', `${Math.round(todayData.tempmin)}°C`);
 
